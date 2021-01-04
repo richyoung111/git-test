5dcbabab3a299621fef70e8b40e41ca43e73f9d3
rich <312yf@163.com>
2021-01-04 15:52:36 +0800

    新建test2方法

--- git-project/src/com/wedge/HelloGit.java
+++ git-project/src/com/wedge/HelloGit.java
@@ -7,10 +7,17 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello git");
         test();
+        test2(20,30);
     }
     public static void test(){
         int i=0;
         i++;
         System.out.println(++i);
     }
+    public static void test2(int a,int b){
+        String str="";
+        System.out.println(a+b);
+    }
+
+
 }
