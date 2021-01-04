d33d7984815a12dc216411bdeb3175936f90c249
rich <312yf@163.com>
2021-01-04 15:25:22 +0800

    本地工程添加到master2

--- git-project/src/com/wedge/HelloGit.java
+++ git-project/src/com/wedge/HelloGit.java
@@ -3,8 +3,13 @@ package com.wedge;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.printf("hello git");
-        System.out.printf("hello git");
-        System.out.printf("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        test();
+    }
+    public static void test(){
+        int i=0;
+        System.out.println(++i);
     }
 }
